108a5c5e48d7bb010b7243099f8d8e2e0bf592f7
Awan1605 <wibokurniawan@gmail.com>
2025-04-13 20:48:51 +0700

    Halaman Home2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('about');
 });
 Route::get('about', [PageController::class, 'about'])->name('about');
